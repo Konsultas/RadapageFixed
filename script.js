db3d5e140ff57fea2c9c22a6468f9077cf296c13
rull3211 <benze00@gmail.com>
2022-08-04 21:38:28 +0200

    feat: back-to-top
    back to top appears on scroll up and disapears on scroll down  and on click

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 $("document").ready(()=>{
-    let barAktiv = false
+    
+    
     const navknapp = $("#navSmallScreen")
     const menu = $(".menu")
-    navknapp.click(function (e) {
-        if(barAktiv){
-            menu.removeClass("menuActive")
-            navknapp.removeClass("navbarSmallScreenActive")
-        }
-        else{
-            navknapp.addClass("navbarSmallScreenActive")
-            menu.addClass("menuActive")
-        }
-        barAktiv=!barAktiv
-       
-    }); 
-    const children = $(".menuPoint")
-    children.click(()=>{
+    const tilToppKanpp =  $(".tilTopp")
+    let barAktiv = false
+    let tilToppAktiv = false
+
+    const resetMenu = ()=>{
         menu.removeClass("menuActive")
         navknapp.removeClass("navbarSmallScreenActive")
         barAktiv=false
+    }
+    const openMenu = ()=>{
+        navknapp.addClass("navbarSmallScreenActive")
+        menu.addClass("menuActive")
+        barAktiv=true
+    }
+
+    navknapp.click(() => {
+        barAktiv ? resetMenu() : openMenu()
+    }); 
+    const children = $(".menuPoint")
+    children.click(()=>{
+        resetMenu()
     })
     $("window").click()
     window.addEventListener('mouseup', (e)=>{
         if(e.target.className !== "hamburgerBar"){   
-            menu.removeClass("menuActive")
-            navknapp.removeClass("navbarSmallScreenActive")
-            barAktiv=false
+            resetMenu()
         }
     });
+
+
+    window.onscroll = ()=>{
+        if(this.oldScroll > this.scrollY && !tilToppAktiv){
+            tilToppKanpp.addClass("tilToppAktiv")
+            tilToppAktiv= true
+        }
+        else if(this.oldScroll < this.scrollY){
+            tilToppKanpp.removeClass("tilToppAktiv")
+            tilToppAktiv=false
+        }
+        this.oldScroll = this.scrollY;
+        }
+
+        tilToppKanpp.click(()=>{
+            tilToppKanpp.removeClass("tilToppAktiv")
+        })
 })
+
 
  
